b7d7ea78c943f1bac2af79b521ad2e0a01301f41
Alexander Adamovskiy <94919988+Alexander0k@users.noreply.github.com>
2022-09-15 00:08:11 +0300

    Завдання 2.3 виконав

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'; 
+import styles from './Statistics.module.css'
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+export default function Statistics({ title, stats }) {
+    return (
+        <section className={styles.statistics}>
+            {title && <h2 className={styles.title}>{title}</h2>}
+
+            <ul className={styles.stat_list}>
+                {stats.map(item => (
+                    <li key={item.id} className={styles.item} style={{ backgroundColor: getRandomHexColor() }}>
+                    <span className={styles.label}>{item.label}</span>
+                    <span className={styles.percentage}>{item.percentage}%</span>
+                    </li>
+                ))}
+            </ul>
+        </section>   
+    );
+};
